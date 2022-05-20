4364fb0829d4974b663a58c0ea9d66a30fc2a5d5
Matheus Costa <matheuscostadosantos@hotmail.com>
2022-05-20 17:58:41 -0300

    Att

--- src/Tmdb.js
+++ src/Tmdb.js
@@ -1,13 +1,19 @@
 const API_KEY = "7c0b7d3bcb99019c5c63096d28e97722";
 const API_BASE = "https://api.themoviedb.org/3";
 
+const basicFetch = async (endpoint) => {
+  const req = await fetch(`${API_BASE}${endpoint}`);
+  const json = await req.json();
+  return json;
+}
+
 export default {
   getHomeList: async () => {
     return [
       {
         slug: "originals",
         title: "Originais do Netflix",
-        items: []
+        items: await basicFetch(`/discover/tv`)
       },
       {
         slug: "trending",
